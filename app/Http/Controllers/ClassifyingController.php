da637a64cb4d6894e531a4c9f32a0778bc74553c
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-06-20 20:25:38 +0300

    some changes

--- app/Http/Controllers/ClassifyingController.php
+++ app/Http/Controllers/ClassifyingController.php
@@ -13,31 +13,13 @@ class ClassifyingController extends Controller
     function start_classify(Request $request){
         if ($request->hasFile('image')) {
             $img_path = Storage::disk('public')->putFile('folder-destination', $request->file('image'));
-
-//            $image      = $request->file('image');
-//            $fileName   = time() . '.' . $image->getClientOriginalExtension();
-//
-//            $img = Image::make($image->getRealPath());
-//            $img->resize(120, 120, function ($constraint) {
-//                $constraint->aspectRatio();
-//            });
-
-//            $img->stream(); // <-- Key point
-
-            //dd();
-//            Storage::disk('public')->put('images/'.'/'.$fileName, $img, 'public');
         }
         if ($request->hasFile('data')){
             $data_path = Storage::disk('public')->putFile('folder-destination', $request->file('data'));
         }
-//        dd("here");
-
-        // /usr/local/my-scripts
-//        $output_data = exec('python3 /usr/local/my-scripts/classify.py /var/www/laravel/storage/app/public/folder-destination/img /var/www/laravel/storage/app/public/folder-destination/data');
         $script = 'python3 /usr/local/my-scripts/classify.py ' . '/var/www/laravel/storage/app/public/'. $img_path . ' ' . '/var/www/laravel/storage/app/public/'.$data_path;
-//        dd($script);
         $output_data = exec($script);
-
+        dd($output_data);
         return view('classify_result');
 
     }
