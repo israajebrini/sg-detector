d46ee3eeaf070ef1b39eba7f3b0c3dc7ba00b09a
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-06-20 23:35:04 +0300

    some changes

--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -231,6 +231,18 @@
         </div>
     </form>
 </div>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $(".btn-success").click(function(){
+            var lsthmtl = $(".clone").html();
+            $(".increment").after(lsthmtl);
+        });
+        $("body").on("click",".btn-danger",function(){
+            $(this).parents(".hdtuto control-group lst").remove();
+        });
+    });
+</script>
+
 </body>
 
 </html>
