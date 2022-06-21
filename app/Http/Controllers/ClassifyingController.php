e62ecf8ad014fabf5e579e036379c66d109c43f4
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-06-21 13:58:30 +0300

    some changes

--- app/Http/Controllers/ClassifyingController.php
+++ app/Http/Controllers/ClassifyingController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use View;
 
 
 class ClassifyingController extends Controller
