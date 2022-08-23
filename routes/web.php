34330d9c4f7b8b50a34ba910e7f0be91f1ec204d
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-08-23 17:11:30 +0300

    chgange home page

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome0');
 })->name('home');
 
 Route::get('/train', function () {
