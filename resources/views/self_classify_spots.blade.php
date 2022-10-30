ba0dfc07b1931e74d70c8078c344717f00b3e30d
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-10-30 23:36:53 +0200

    added self classify

--- resources/views/self_classify_spots.blade.php
+++ resources/views/self_classify_spots.blade.php
@@ -33,10 +33,18 @@
 </div>
 
 <div class="TrainOrClassify">
-    <div class="image-container">
-        <img id="image" src="{{ url('storage/'.$image->path) }}"  style="display: block;max-width: 100% ; border: 20px solid #4CAF50">
+{{--    <div class="image-container">--}}
+{{--        <img id="image" src="{{ url('storage/'.$image->path) }}"  style="display: block;max-width: 100% ; border: 20px solid #4CAF50">--}}
+{{--    </div>--}}
+{{--    <button id="send" >Send</button>--}}
+
+    <input type="file" name="image" id="image" onchange="readURL(this);"/>
+    <div class="image_container">
+        <img id="blah" src="#" alt="your image" />
     </div>
-    <button id="send" >Send</button>
+    <div id="cropped_result"></div>        // Cropped image to display (only if u want)
+    <button id="crop_button">Crop</button> // Will trigger crop event
+
 {{--    <form action="{{route('save_image')}}" method="post" enctype="multipart/form-data">--}}
 {{--        @csrf--}}
 {{--        <fieldset style="display: flex;--}}
@@ -58,68 +66,117 @@
 <script src="https://unpkg.com/bootstrap@4/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/cropperjs/1.5.12/cropper.js" integrity="sha512-ZK6m9vADamSl5fxBPtXw6ho6A4TuX89HUbcfvxa2v2NYNT/7l8yFGJ3JlXyMN4hlNbz0il4k6DvqbIW5CCwqkw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script>
-    window.addEventListener('DOMContentLoaded', function () {
-        var image = document.getElementById('image');
-        var cropper = new Cropper(image, {
-            aspectRatio: 1,
-            viewMode: 3,
-        });
-        if (cropper) {
-            canvas = cropper.getCroppedCanvas({
-                width: 160,
-                height: 160,
-            });
-
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                $('#blah').attr('src', e.target.result)
+            };
+            reader.readAsDataURL(input.files[0]);
+            setTimeout(initCropper, 1000);
         }
+    }
+    function initCropper(){
+        var image = document.getElementById('blah');
+        var cropper = new Cropper(image, {
+            aspectRatio: 1 / 1,
+            crop: function(e) {
+                console.log(e.detail.x);
+                console.log(e.detail.y);
+            }
+        });
 
-        document.getElementById('send').addEventListener('click', function () {
-            initialAvatarURL = avatar.src;
-            avatar.src = canvas.toDataURL();
+        // On crop button clicked
+        document.getElementById('crop_button').addEventListener('click', function(){
+            var imgurl =  cropper.getCroppedCanvas().toDataURL();
+            var img = document.createElement("img");
+            img.src = imgurl;
+            document.getElementById("cropped_result").appendChild(img);
 
-            canvas.toBlob(function (blob) {
-                var formData = new FormData();
 
-                formData.append('avatar', blob, 'avatar.jpg');
-                $.ajax('http://206.81.20.227/posts', {
-                    method: 'POST',
-                    data: formData,
-                    processData: false,
-                    contentType: false,
 
-                    xhr: function () {
-                        var xhr = new XMLHttpRequest();
-
-                        xhr.upload.onprogress = function (e) {
-                            var percent = '0';
-                            var percentage = '0%';
-
-                            if (e.lengthComputable) {
-                                percent = Math.round((e.loaded / e.total) * 100);
-                                percentage = percent + '%';
-                                $progressBar.width(percentage).attr('aria-valuenow', percent).text(percentage);
-                            }
-                        };
-
-                        return xhr;
-                    },
-
-                    success: function () {
-                        $alert.show().addClass('alert-success').text('Upload success');
-                    },
-                    error: function () {
-                        avatar.src = initialAvatarURL;
-                        $alert.show().addClass('alert-warning').text('Upload error');
-                    },
-
-                    complete: function () {
-                        $progress.hide();
-                    },
+                cropper.getCroppedCanvas().toBlob(function (blob) {
+                      var formData = new FormData();
+                      formData.append('croppedImage', blob);
+                      // Use `jQuery.ajax` method
+                      $.ajax('http://206.81.20.227/posts', {
+                        method: "POST",
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function () {
+                          console.log('Upload success');
+                        },
+                        error: function () {
+                          console.log('Upload error');
+                        }
+                      });
                 });
-            });
-        });
 
-
-        });
+        })
+    }
+    // window.addEventListener('DOMContentLoaded', function () {
+    //     var image = document.getElementById('image');
+    //     var cropper = new Cropper(image, {
+    //         aspectRatio: 1,
+    //         viewMode: 3,
+    //     });
+    //     if (cropper) {
+    //         canvas = cropper.getCroppedCanvas({
+    //             width: 160,
+    //             height: 160,
+    //         });
+    //
+    //     }
+    //
+    //     document.getElementById('send').addEventListener('click', function () {
+    //         initialAvatarURL = avatar.src;
+    //         avatar.src = canvas.toDataURL();
+    //
+    //         canvas.toBlob(function (blob) {
+    //             var formData = new FormData();
+    //
+    //             formData.append('avatar', blob, 'avatar.jpg');
+    //             $.ajax('http://206.81.20.227/posts', {
+    //                 method: 'POST',
+    //                 data: formData,
+    //                 processData: false,
+    //                 contentType: false,
+    //
+    //                 xhr: function () {
+    //                     var xhr = new XMLHttpRequest();
+    //
+    //                     xhr.upload.onprogress = function (e) {
+    //                         var percent = '0';
+    //                         var percentage = '0%';
+    //
+    //                         if (e.lengthComputable) {
+    //                             percent = Math.round((e.loaded / e.total) * 100);
+    //                             percentage = percent + '%';
+    //                             $progressBar.width(percentage).attr('aria-valuenow', percent).text(percentage);
+    //                         }
+    //                     };
+    //
+    //                     return xhr;
+    //                 },
+    //
+    //                 success: function () {
+    //                     $alert.show().addClass('alert-success').text('Upload success');
+    //                 },
+    //                 error: function () {
+    //                     avatar.src = initialAvatarURL;
+    //                     $alert.show().addClass('alert-warning').text('Upload error');
+    //                 },
+    //
+    //                 complete: function () {
+    //                     $progress.hide();
+    //                 },
+    //             });
+    //         });
+    //     });
+    //
+    //
+    //     });
 </script>
 </html>
 
