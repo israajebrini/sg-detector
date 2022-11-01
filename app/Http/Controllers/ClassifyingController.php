1382a4d486df7ea7bef12628c5848da3eda89a71
zahi204 <63948209+zahi204@users.noreply.github.com>
2022-11-01 03:10:28 +0200

    added self classify

--- app/Http/Controllers/ClassifyingController.php
+++ app/Http/Controllers/ClassifyingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\BaseImage;
+use App\Models\Spot;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Process\Process;
